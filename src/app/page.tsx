5414b1e3a3126a2216787a2c820bd469f9fdba42
unknown <hristodanchev01@gmail.com>
2026-02-13 19:13:02 +0100

    added links to sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import RichTextEditor from "@/components/text_editor/RichTextEditor";
 
 export default function Home() {
   return (
     <>
-      <RichTextEditor />
     </>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import BellIcon from "./icons/BellIcon";
 import HomeSmileIcon from "./icons/HomeSmileIcon";
 import PlusIcon from "./icons/PlusIcon";
+import Link from "next/link";
 
 import {
   Tooltip,
@@ -16,9 +17,11 @@ const Sidebar = () => {
         <TooltipProvider>
           <Tooltip delayDuration={0}>
             <TooltipTrigger asChild>
-              <button className="group flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-[0.75rem] transition duration-200 hover:bg-slate-100">
-                <HomeSmileIcon className="h-[2rem] w-[2rem] fill-slate-400 transition duration-200 group-hover:fill-blue-500" />
-              </button>
+              <Link href="/posts">
+                <button className="group flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-[0.75rem] transition duration-200 hover:bg-slate-100">
+                  <HomeSmileIcon className="h-[2rem] w-[2rem] fill-slate-400 transition duration-200 group-hover:fill-blue-500" />
+                </button>
+              </Link>
             </TooltipTrigger>
             <TooltipContent side="right" sideOffset={8} className="max-w-xs">
               {"Home"}
@@ -28,9 +31,11 @@ const Sidebar = () => {
         <TooltipProvider>
           <Tooltip delayDuration={0}>
             <TooltipTrigger asChild>
-              <button className="group flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-[0.75rem] transition duration-200 hover:bg-slate-100">
-                <BellIcon className="h-[2rem] w-[2rem] fill-slate-400 stroke-slate-400 transition duration-200 group-hover:fill-blue-500 group-hover:stroke-blue-500" />
-              </button>
+              <Link href="/user">
+                <button className="group flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-[0.75rem] transition duration-200 hover:bg-slate-100">
+                  <BellIcon className="h-[2rem] w-[2rem] fill-slate-400 stroke-slate-400 transition duration-200 group-hover:fill-blue-500 group-hover:stroke-blue-500" />
+                </button>
+              </Link>
             </TooltipTrigger>
             <TooltipContent side="right" sideOffset={8} className="max-w-xs">
               {"Activity"}
@@ -40,9 +45,11 @@ const Sidebar = () => {
         <TooltipProvider>
           <Tooltip delayDuration={0}>
             <TooltipTrigger asChild>
-              <button className="my-[0.5rem] flex h-[2.75rem] w-[2.75rem] items-center justify-center rounded-[0.75rem] bg-slate-400 transition duration-200 hover:bg-blue-500">
-                <PlusIcon className="h-[1.375rem] w-[1.375rem] fill-white stroke-white stroke-[3]" />
-              </button>
+              <Link href="/write">
+                <button className="my-[0.5rem] flex h-[2.75rem] w-[2.75rem] items-center justify-center rounded-[0.75rem] bg-slate-400 transition duration-200 hover:bg-blue-500">
+                  <PlusIcon className="h-[1.375rem] w-[1.375rem] fill-white stroke-white stroke-[3]" />
+                </button>
+              </Link>
             </TooltipTrigger>
             <TooltipContent side="right" sideOffset={14} className="max-w-xs">
               {"New Post"}
